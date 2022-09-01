2570af40209f37b77d7cf6130b04d15626b3a2b6
Alex A <alex.av936@gmail.com>
2022-08-31 22:47:14 -0500

    changed to new url

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 //For API calls pertaining a single user
 export const postUserActivity = async (userData) => {
-  return axios.post("http://flask-env.eba-pqxgz2ka.us-east-2.elasticbeanstalk.com/insertActivity", userData, {headers: {
+  return axios.post("http://mondayapiv2-env.eba-2dwymyer.us-east-2.elasticbeanstalk.com/insertActivity", userData, {headers: {
     'content-type': 'application/json'
   }}
   )
@@ -11,7 +11,7 @@ export const postUserActivity = async (userData) => {
 
 //total points for user, no dates
 export const getSingleUserTotalPoints = async (userId) => {
-  return axios.get(`http://flask-env.eba-pqxgz2ka.us-east-2.elasticbeanstalk.com/getSingleUserTotalPoints/${userId}`, {headers: {
+  return axios.get(`http://mondayapiv2-env.eba-2dwymyer.us-east-2.elasticbeanstalk.com/getSingleUserTotalPoints/${userId}`, {headers: {
       'content-type': 'application/json'
     }}
   ).then(response => response.data);
@@ -19,7 +19,7 @@ export const getSingleUserTotalPoints = async (userId) => {
 
 //Returns amount of points for month with date being then end of that week ex: 2022-08-28
 export const getSingleUserTotalPointsPerMonthByWeek = async (userId) => {
-  return axios.get(`http://flask-env.eba-pqxgz2ka.us-east-2.elasticbeanstalk.com/getSingleUserTotalPointsPerMonthByWeek/${userId}`, {headers: {
+  return axios.get(`http://mondayapiv2-env.eba-2dwymyer.us-east-2.elasticbeanstalk.com/getSingleUserTotalPointsPerMonthByWeek/${userId}`, {headers: {
       'content-type': 'application/json'
     }}
   ).then(response =>  response.data);
@@ -27,7 +27,7 @@ export const getSingleUserTotalPointsPerMonthByWeek = async (userId) => {
 
 //Returns amount of points for month with date being then end of that month ex: 2022-08-31
 export const getSingleUserTotalPointsPerYearByMonth = async (userId) => {
-  return axios.get(`http://flask-env.eba-pqxgz2ka.us-east-2.elasticbeanstalk.com/getSingleUserTotalPointsPerYearByMonth/${userId}`, {headers: {
+  return axios.get(`http://mondayapiv2-env.eba-2dwymyer.us-east-2.elasticbeanstalk.com/getSingleUserTotalPointsPerYearByMonth/${userId}`, {headers: {
       'content-type': 'application/json'
     }}
   ).then(response =>  response.data);
@@ -35,14 +35,14 @@ export const getSingleUserTotalPointsPerYearByMonth = async (userId) => {
 
 //Returns amount of points for month by week by activity
 export const getSingleUserTotalPointsPerWeekPerActivity = async (userId) => {
-  return axios.get(`http://flask-env.eba-pqxgz2ka.us-east-2.elasticbeanstalk.com/getSingleUserTotalPointsPerWeekPerActivity/${userId}`, {headers: {
+  return axios.get(`http://mondayapiv2-env.eba-2dwymyer.us-east-2.elasticbeanstalk.com/getSingleUserTotalPointsPerWeekPerActivity/${userId}`, {headers: {
       'content-type': 'application/json'
     }}
   ).then(response =>  response.data);
 };
 //Returns amount of points for a single user YTD
 export const getSingleUserTotalPointsYTDByActivity = async (userId) => {
-    return axios.get(`http://flask-env.eba-pqxgz2ka.us-east-2.elasticbeanstalk.com/getSingleUserTotalPointsYTDByActivity/${userId}`, {headers: {
+    return axios.get(`http://mondayapiv2-env.eba-2dwymyer.us-east-2.elasticbeanstalk.com/getSingleUserTotalPointsYTDByActivity/${userId}`, {headers: {
             'content-type': 'application/json'
         }}
     ).then(response =>  console.log(response.data));
